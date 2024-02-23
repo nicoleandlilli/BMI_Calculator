fdbee9408d10efd5771132cefefe5de7cbc5845e
wangguiye <wangguiye@lenovo.com>
2024-02-23 21:47:18 +0800

    使用三目运算符实现点击控件后颜色的更改

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -23,28 +23,8 @@ class InputPage extends StatefulWidget{
 
 class _InputPageState extends State<InputPage>{
 
-  Color maleCardColor = inactiveCardColor;
-  Color femaleCardColor = inactiveCardColor;
+  Gender selectedGender = Gender.female;
 
-  //1 = male, 2 = female
-  void updateColor(Gender selectedGender){
-    //male card pressed
-    if(selectedGender == Gender.male){
-      if(maleCardColor == inactiveCardColor){
-        maleCardColor = activeCardColor;
-        femaleCardColor = inactiveCardColor;
-      }else{
-        maleCardColor = inactiveCardColor;
-      }
-    }else if(selectedGender == Gender.female){
-      if(femaleCardColor == inactiveCardColor){
-        femaleCardColor = activeCardColor;
-        maleCardColor = inactiveCardColor;
-      }else{
-        femaleCardColor = inactiveCardColor;
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,11 +48,11 @@ class _InputPageState extends State<InputPage>{
               Expanded(child:GestureDetector(
                 onTap: (){
                   setState(() {
-                    updateColor(Gender.male);
+                    selectedGender = Gender.male;
                   });
                 },
                 child:  ReusableCard(
-                  color: maleCardColor,
+                  color: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
                   cardChild: IconContent(icon: FontAwesomeIcons.mars,label: "MALE",),
                 ),
               ),
@@ -80,11 +60,11 @@ class _InputPageState extends State<InputPage>{
               Expanded(child: GestureDetector(
                 onTap: (){
                   setState(() {
-                    updateColor(Gender.female);
+                    selectedGender = Gender.female;
                   });
                 },
                 child: ReusableCard(
-                  color: femaleCardColor,
+                  color: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
                   cardChild: IconContent(icon: FontAwesomeIcons.venus,label: "FEMALE",),
                 ),
               ),

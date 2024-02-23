c8d3b044b52fcf032a2f9c0363e1ff4d13785aa1
wangguiye <wangguiye@lenovo.com>
2024-02-24 04:22:11 +0800

    将常量移动到单独的dart文件中

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,10 +1,6 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
 
-import 'package:flutter/cupertino.dart';
-
-const labelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
 
 class IconContent extends StatelessWidget {
 
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
+import 'constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
-const bottomContainerHeight = 80.0;
-const bottomContainerColor = Color(0xFFEB1555);
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
+
 
 enum Gender{
   male,
@@ -51,7 +49,7 @@ class _InputPageState extends State<InputPage>{
                     selectedGender = Gender.male;
                   });
                 },
-                color: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
+                color: selectedGender == Gender.male ? kActiveCardColor : kInactiveCardColor,
                 cardChild: IconContent(icon: FontAwesomeIcons.mars,label: "MALE",),
               ),
               ),
@@ -61,7 +59,7 @@ class _InputPageState extends State<InputPage>{
                     selectedGender = Gender.female;
                   });
                 },
-                color: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
+                color: selectedGender == Gender.female ? kActiveCardColor : kInactiveCardColor,
                 cardChild: IconContent(icon: FontAwesomeIcons.venus,label: "FEMALE",),
               ),
               ),
@@ -73,7 +71,7 @@ class _InputPageState extends State<InputPage>{
                   selectedGender = Gender.male;
                 });
               },
-              color: activeCardColor),),
+              color: kActiveCardColor),),
           Expanded(child: Row(
             children: [
               Expanded(child: ReusableCard(
@@ -82,21 +80,21 @@ class _InputPageState extends State<InputPage>{
                       selectedGender = Gender.male;
                     });
                   },
-                  color: activeCardColor),),
+                  color: kActiveCardColor),),
               Expanded(child: ReusableCard(
                   onPress: (){
                     setState(() {
                       selectedGender = Gender.male;
                     });
                   },
-                  color: activeCardColor),),
+                  color: kActiveCardColor),),
             ],
           )),
           Container(
-            color: bottomContainerColor,
+            color: kBottomContainerColor,
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           )
         ],
       )
